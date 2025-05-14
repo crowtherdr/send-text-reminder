9201dbe69b3c6d1d03cde2902891c57b764a8e2c
David Crowther <crowtherdr@gmail.com>
2025-05-14 09:24:58 -0600

    Adds latest msg update

--- src/App.js
+++ src/App.js
@@ -4,32 +4,30 @@ import Papa from "papaparse";
 const SmsSender = () => {
   const [contacts, setContacts] = useState([]);
   // eslint-disable-next-line no-multi-str
-  const message = "Hi {name}, \n\
-This is a reminder that tomorrow night at 8 p.m. we have our Lehi CERT leadership mtg.\n\
-\n\
-This meeting will be held on Teams: https://teams.microsoft.com/l/meetup-join/19%3ameeting_OTJhNmJmYmYtMDA1ZS00MDQ0LWJhZmMtNGI0MmJhNjM1YjI4%40thread.v2/0?context=%7b%22Tid%22%3a%221d19effd-6615-4b00-a2e1-fa79a5f92d32%22%2c%22Oid%22%3a%2261c6b800-44fc-4eb8-b01d-1addaf649dc1%22%7d\n\
-\n\
-We have several agenda items.\n\
-\n\
-Best regards—\n\
-David Crowther\n\
-Lehi CERT Secretary";
 //   const message = "Hi {name}, \n\
-// This is a reminder that tonight at 7 p.m. we have our Lehi CERT Team mtg.\n\
+// This is a reminder that tomorrow night at 8 p.m. we have our Lehi CERT leadership mtg.\n\
 // \n\
-// This meeting will be held in person at Lehi Police Station Broadbent Room on 128 North 100 East\n\
+// This meeting will be held on Teams: https://teams.microsoft.com/l/meetup-join/19%3ameeting_OTJhNmJmYmYtMDA1ZS00MDQ0LWJhZmMtNGI0MmJhNjM1YjI4%40thread.v2/0?context=%7b%22Tid%22%3a%221d19effd-6615-4b00-a2e1-fa79a5f92d32%22%2c%22Oid%22%3a%2261c6b800-44fc-4eb8-b01d-1addaf649dc1%22%7d\n\
 // \n\
-// We will have an earthquake preparedness seminar\n\
-// AFTER the seminar we will discuss our upcoming drill on April 16th\n\
-// WHILE we are doing that, Steve Boyack will program anyone’s Baofeng radio\n\
-// that wants our current CERT program list on their radio.\n\
-// This will have all Lehi CERT frequencies, Lehi ARC, UCARC, UCARES frequencies as well (all told about 30 channels).\n\
-// If you want this done, please respond to Steve's Slack thread in the discussion-general  channel with the model of your radio.\n\
+// We have several agenda items.\n\
 // \n\
 // Best regards—\n\
 // David Crowther\n\
 // Lehi CERT Secretary";
-  const csvFilePath = "/csv/cert_leaders_fname_phone.csv";
+// const csvFilePath = "/csv/cert_leaders_fname_phone.csv";
+// eslint-disable-next-line no-multi-str
+  const message = "Hi {name}, \n\
+This is a reminder that tonight at 7 p.m. we have our Lehi CERT Team mtg.\n\
+\n\
+This meeting will be held in person at Lehi Fire Station #82 conference room on 250 W 2600 N\n\
+\n\
+Lehi CERT presents local training for STOP THE BLEED\n\
+Everyone invited\n\
+\n\
+Best regards—\n\
+David Crowther\n\
+Lehi CERT Secretary";
+  const csvFilePath = "/csv/cert_fname_phone.csv";
 
   useEffect(() => {
     const sentContacts = JSON.parse(localStorage.getItem("sentContacts")) || [];
