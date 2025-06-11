a10159d81f364889852dacd070ef22766c53c394
David Crowther <crowtherdr@gmail.com>
2025-06-11 15:45:31 -0600

    Saves latest reminders
    Possible tasks:
    * Put reminders in a separate file.
    * Add reminder or option to clear the localstorage before sending msgs

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ const SmsSender = () => {
   const [contacts, setContacts] = useState([]);
   // eslint-disable-next-line no-multi-str
 //   const message = "Hi {name}, \n\
-// This is a reminder that tomorrow night at 8 p.m. we have our Lehi CERT leadership mtg.\n\
+// This is a reminder that tonight night at 8 p.m. we have our Lehi CERT leadership mtg.\n\
 // \n\
 // This meeting will be held on Teams: https://teams.microsoft.com/l/meetup-join/19%3ameeting_OTJhNmJmYmYtMDA1ZS00MDQ0LWJhZmMtNGI0MmJhNjM1YjI4%40thread.v2/0?context=%7b%22Tid%22%3a%221d19effd-6615-4b00-a2e1-fa79a5f92d32%22%2c%22Oid%22%3a%2261c6b800-44fc-4eb8-b01d-1addaf649dc1%22%7d\n\
 // \n\
-// We have several agenda items.\n\
+// Please join us.\n\
 // \n\
 // Best regards—\n\
 // David Crowther\n\
@@ -21,9 +21,18 @@ This is a reminder that tonight at 7 p.m. we have our Lehi CERT Team mtg.\n\
 \n\
 This meeting will be held in person at Lehi Fire Station #82 conference room on 250 W 2600 N\n\
 \n\
-Lehi CERT presents local training for STOP THE BLEED\n\
-Everyone invited\n\
+We plan on starting the Emergency Management Institute (EMI) online course IS-100.C: Introduction to the Incident Command System (ICS 100) together.\n\
+https://training.fema.gov/is/courseoverview.aspx?code=IS-100.c&lang=en\n\
 \n\
+The course is about 2 hours. We will do as much as we can together.\n\
+\n\
+Before Wednesday night, please create your own SID (Student ID) login: https://cdp.dhs.gov/femasid/register\n\
+\n\
+Please bring your own laptop so you can get credit for the course.\n\
+\n\
+Even if you have completed the course IS–100, we are going to review some other CERT business and hope you will still join us.\n\
+\n\
+Please join us as we each increase our capabilities and increase our team's capacity.\n\
 Best regards—\n\
 David Crowther\n\
 Lehi CERT Secretary";
