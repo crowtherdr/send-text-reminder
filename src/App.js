fa5935f6591928fb14dbd304a3ac518d6d6aa9a7
David Crowther <crowtherdr@gmail.com>
2025-12-01 15:19:03 -0600

    Adds December info

--- src/App.js
+++ src/App.js
@@ -5,45 +5,44 @@ const SmsSender = () => {
   const [contacts, setContacts] = useState([]);
 
 // eslint-disable-next-line no-multi-str
-//   const message = "Hi {name}, \n\
-// This is a reminder that there will not be Lehi CERT leadership mtg in July.\n\
-// \n\
-// If you have any leadership business, post it in the Slack \"leadership\" channel.\n\
-// \n\
-// Please join us.\n\
-// \n\
-// Best regards—\n\
-// David Crowther\n\
-// Lehi CERT Secretary";
-
-  // eslint-disable-next-line no-multi-str
   const message = "Hi {name}, \n\
-Our leadership meeting will be a bit different in October as it is the same evening as the Lehi Fire Department open house at Wines Park at 100 E 600 N St, Lehi, Utah. We will have a CERT booth there and need your help to staff the booth.\n\
+Please join us for our monthly CERT leadership team meeting and pizza, Wednesday, 10 December at 6:30p at Steve & Merrilee Boyack's home (2847 N, 50 W., Lehi, Utah). Add agenda items to the mtg announcement in the Slack #leadership channel. Also, PLEASE RSVP to the Slack #leadership channel with a 👍 if you are coming so we have enough pizza (https://lehicert.slack.com/archives/C057EKUC2CE/p1764542632349009)\n\
 \n\
-We would like to have 2 shifts from 3:30pm-5:15pm and 5:15p-7pm. That's 1.75 hours each. If you can come for only part of the time, that will also help. We need 3 people per shift.\n\
-\n\
-Here is a link to the signup sheet: https://docs.google.com/spreadsheets/d/1pzavmSE48MMrdG1sXdu1sDonHT9emOxifI5H2Fc8bkE/edit?usp=sharing\n\
-\n\
-We will try to meet during the end of the first shift and the start of the 2nd shift, perhaps from 5pm-6pm.\n\
-\n\
-Send agenda items to Brian Sump, Steve Boyack, or me, or add them to the mtg announcement in the Slack #leadership channel: https://lehicert.slack.com/archives/C057EKUC2CE/p1758760068165199\n\
-\n\
-Please note that we just found out about this today. Please pardon any changes as we get closer to the event.\n\
-\n\
-Thank you.\n\
+Please join us.\n\
 \n\
 Best regards—\n\
 David Crowther\n\
 Lehi CERT Secretary";
+
+  // eslint-disable-next-line no-multi-str
+//   const message = "Hi {name}, \n\
+// Last reminder for this evening. \n\
+// Our leadership meeting will be a bit different in October as it is the same evening as the Lehi Fire Department open house at Wines Park at 100 E 600 N St, Lehi, Utah. We will have a CERT booth there and need your help to staff the booth.\n\
+// \n\
+// If you signed up to help, or end up helping in the booth, thank you.\n\
+// \n\
+// Here is a link to the signup sheet: https://docs.google.com/spreadsheets/d/1pzavmSE48MMrdG1sXdu1sDonHT9emOxifI5H2Fc8bkE/edit?usp=sharing\n\
+// \n\
+// We will try to meet during the end of the first shift and the start of the 2nd shift, perhaps from 5pm-6pm.\n\
+// \n\
+// Send agenda items to Brian Sump, Steve Boyack, or me, or add them to the mtg announcement in the Slack #leadership channel: https://lehicert.slack.com/archives/C057EKUC2CE/p1758760068165199\n\
+// \n\
+// Thank you.\n\
+// \n\
+// Best regards—\n\
+// David Crowther\n\
+// Lehi CERT Secretary";
 const csvFilePath = "/csv/cert_leaders_fname_phone.csv";
 
 // eslint-disable-next-line no-multi-str
 //   const message = "Hi {name}, \n\
-// This is a reminder that tonight at 7 p.m. we have our Lehi CERT Team mtg.\n\
+// This is a reminder that tomorrow night at 7 p.m. we have our Lehi CERT Team mtg.\n\
 // \n\
-// This meeting will be held in person at Lehi Police Station conference room on 128 N 100 E\n\
+// This meeting will be held in person at 250 W 2600 N (Fire Station #82 conference room)\n\
 // \n\
-// September is National Preparedness Month. We will be talking about general preparedness and terrorism awareness.\n\
+// I am sending this reminder a day earlier because of the elections nomination deadline of today at 5 p.m. Please see the emails I sent last Wednesday and yesterday for the details.\n\
+// We will hold elections for our 2026-2027 Lehi CERT program manager and assistant program manager.\n\
+// Then we will have a presentation about winter preparedness with some emphasis on CERT winter preparedness.\n\
 // \n\
 // We hope to see you there!\n\
 // \n\
